a0f47d1a6a3d6c05c9c964f6ecc7466c09517b40
Shehab Anwar <126429412+Shehab-Anwar-X@users.noreply.github.com>
2023-09-15 14:47:41 +0300

    add BuiltInDataType interface

--- lib/src/data_type.dart
+++ lib/src/data_type.dart
@@ -8,29 +8,36 @@ abstract class DataType<T> {
 }
 
 /// Represent the primitive data types like int, double, String, bool, ..etc.
-abstract class PrimitiveDataType<T> implements DataType<T>, NullableDataType {
+abstract class PrimitiveDataType<T>
+    implements DataType<T>, NullableDataType, BuiltInDataType {
   const PrimitiveDataType();
 }
 
 /// Represent the collections data types like List and Map
-abstract class CollectionDataType<T> implements DataType<T>, NullableDataType {
+abstract class CollectionDataType<T>
+    implements DataType<T>, NullableDataType, BuiltInDataType {
   const CollectionDataType();
 }
 
 /// Represent the customized types
 /// any new type you want to create when the application running.
-abstract class CustomizeDataType implements DataType<dynamic>, NullableDataType {
+abstract class CustomizeDataType
+    implements DataType<dynamic>, NullableDataType {
   const CustomizeDataType();
 
   String get dataType;
 }
 
 /// Represent the Future and Stream types.
-abstract class AsynchronousDataType<T> implements DataType<T>, NullableDataType {
+abstract class AsynchronousDataType<T>
+    implements DataType<T>, NullableDataType, BuiltInDataType {
   const AsynchronousDataType();
 }
 
 /// Represent the special data types like void and dynamic types.
-abstract class SpecialDataTypes<T> implements DataType<T> {
+abstract class SpecialDataTypes<T> implements DataType<T>, BuiltInDataType {
   const SpecialDataTypes();
 }
+
+/// Built-in types
+abstract class BuiltInDataType {}
